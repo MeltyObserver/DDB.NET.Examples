9ae4fd538a507f048a9f57c57df7cdf48a294158
MeltyObserver <164113402+MeltyObserver@users.noreply.github.com>
2024-11-14 16:25:27 +0100

    added example of ADO.net working

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Data.Common;
 using Dapper;
 using DuckDB.NET.Data;
 
@@ -20,26 +21,77 @@ using (var cn = new DuckDBConnection(connectionString))
 
     Console.WriteLine("DuckDB version: {0}\n", cn.ServerVersion);
 
-    cn.Execute("CREATE TABLE files (fileType INTEGER, creationDate DATE, title VARCHAR, extras STRUCT(v VARCHAR, i VARCHAR));");
+    cn.Execute("CREATE TABLE files (title VARCHAR, fileType INTEGER, creationDate DATE, extras STRUCT(v VARCHAR, i VARCHAR));");
 
-    Console.WriteLine("--- attempting to insert a date ---\n");
+    cn.Execute("INSERT INTO files (title, creationDate) VALUES ($title, $date)",
+        new { title = file.Title, date = file.CreationDate });
 
-    cn.Execute("INSERT INTO files (creationDate, title) VALUES ($date, $title)",
-        new { date = file.CreationDate, title = file.Title });
-
-    var query = cn.Query<MyFileClass>("SELECT * FROM files");
-
-    foreach (var q in query)
+    try
     {
-        Console.WriteLine($"{q.CreationDate} ");
+        Console.WriteLine("--- attempting to fetch with dapper ---\n");
+        var query = cn.Query<MyFileClass>("SELECT * FROM files");
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+        Console.WriteLine("--------------");
+        Console.WriteLine(e.InnerException);
+        Console.WriteLine("--------------");
+        Console.WriteLine(e.StackTrace);
+    }
+
+    try
+    {
+        Console.WriteLine("\n--- attempting to fetch with ADO ---\n");
+
+        using var command = cn.CreateCommand();
+        command.CommandText = "SELECT * FROM files;";
+        var reader = command.ExecuteReader();
+        PrintQueryResults(reader);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+        Console.WriteLine("--------------");
+        Console.WriteLine(e.InnerException);
+        Console.WriteLine("--------------");
+        Console.WriteLine(e.StackTrace);
+    }
+}
+
+static void PrintQueryResults(DbDataReader queryResult)
+{
+    for (var index = 0; index < queryResult.FieldCount; index++)
+    {
+        var column = queryResult.GetName(index);
+        Console.Write($"{column} ");
+    }
+
+    Console.WriteLine();
+
+    while (queryResult.Read())
+    {
+        for (int ordinal = 0; ordinal < queryResult.FieldCount; ordinal++)
+        {
+            if (queryResult.IsDBNull(ordinal))
+            {
+                Console.WriteLine("NULL");
+                continue;
+            }
+            var val = queryResult.GetValue(ordinal);
+            Console.Write(val);
+            Console.Write(" ");
+        }
+
+        Console.WriteLine();
     }
 }
 
 public class MyFileClass
 {
+    public string? Title { get; set; }
     public MyFileType Type { get; set; }
     public DateTime CreationDate { get; set; }
-    public string? Title { get; set; }
     public Dictionary<string, string>? Extras { get; set; }
 }
 public enum MyFileType
